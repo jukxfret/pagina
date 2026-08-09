6257aa629faa814573f637cf9b5b976662e85538
jukxfret <juancamilolandinez7@gmail.com>
2026-08-08 22:25:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function() {
-    const forms = document.querySelectorAll('form');
-    
-    forms.forEach(form => {
-        form.addEventListener('submit', function(event) {
-            event.preventDefault();
-            alert('Formulario enviado con éxito.');
-            // Aquí puedes agregar código para enviar datos al backend
-        });
-    });
-});
+
